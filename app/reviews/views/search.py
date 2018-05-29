1030970d92ff6563b964db0f47d49c882e34119c
rainsound-k <rainsound128@gmail.com>
2018-05-29 13:09:20 +0900

    search-review pagination 적용

--- app/reviews/views/search.py
+++ app/reviews/views/search.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render
 
@@ -7,25 +8,39 @@ from reviews.models import Review
 def search_review(request):
     search_text = request.GET.get('search-text')
     search_type = request.GET.get('search-type')
-    context = {}
+
+    # search 후 다음 페이지 넘겨도 search 정보 유지
+    get_copy = request.GET.copy()
+    parameters = get_copy.pop('page', True) and get_copy.urlencode()
 
     if search_text:
         if search_type == 'title':
-            reviews = Review.objects.filter(title__contains=search_text)
+            reviews_list = Review.objects.filter(title__contains=search_text)
         elif search_type == 'content':
-            reviews = Review.objects.filter(content__contains=search_text)
+            reviews_list = Review.objects.filter(content__contains=search_text)
         elif search_type == 'item':
-            reviews = Review.objects.filter(item__name__contains=search_text)
+            reviews_list = Review.objects.filter(item__name__contains=search_text)
         else:
-            reviews = Review.objects.filter(
+            reviews_list = Review.objects.filter(
                 Q(title__contains=search_text) | Q(content__contains=search_text) | Q(item__name__contains=search_text)
             )
+        paginator = Paginator(reviews_list, 30)
+        paginator_num = range(1, paginator.num_pages + 1)
+        page = request.GET.get('page')
+        reviews = paginator.get_page(page)
+
     else:
-        reviews = Review.objects.all()
+        reviews_list = Review.objects.all()
+        paginator = Paginator(reviews_list, 30)
+        paginator_num = range(1, paginator.num_pages + 1)
+        page = request.GET.get('page')
+        reviews = paginator.get_page(page)
 
     context = {
         'reviews': reviews,
         'search_text': search_text,
+        'paginator_num': paginator_num,
+        'parameters': parameters,
     }
 
     return render(request, 'reviews/review_list.html', context)
