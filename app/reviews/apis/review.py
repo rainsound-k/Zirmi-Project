3ad3e454da5306a599fa0b67cbe8c661ca4bebd3
rainsound-k <rainsound128@gmail.com>
2018-06-14 11:45:10 +0900

    ReviewSearchFromKeyword api 구현 및 url 연결

--- app/reviews/apis/review.py
+++ app/reviews/apis/review.py
@@ -1,4 +1,7 @@
-from rest_framework import generics, permissions
+from django.db.models import Q
+from django.utils.datastructures import MultiValueDictKeyError
+from rest_framework import generics, permissions, exceptions
+from rest_framework.response import Response
 
 from utils.pagination import SmallPagination
 from utils.permissions import IsUserOrReadOnly
@@ -8,6 +11,7 @@ from ..models import Review
 __all__ = (
     'ReviewListCreateView',
     'ReviewRetrieveUpdateDestroyView',
+    'ReviewSearchFromKeyword',
 )
 
 
@@ -30,3 +34,45 @@ class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (
         IsUserOrReadOnly,
     )
+
+
+class ReviewSearchFromKeyword(generics.ListAPIView):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        try:
+            sort = self.request.query_params['sort']
+            keyword = self.request.query_params['keyword']
+        except MultiValueDictKeyError:
+            data = {
+                'detail': 'sort와 keyword를 확인해주세요'
+            }
+            raise exceptions.ValidationError(data)
+        else:
+            if keyword:
+                if sort == 'title':
+                    return Review.objects.filter(title__contains=keyword)
+                elif sort == 'content':
+                    return Review.objects.filter(content__contains=keyword)
+                elif sort == 'item':
+                    return Review.objects.filter(item__name__contains=keyword)
+                elif sort == 'all':
+                    return Review.objects.filter(
+                        Q(title__contains=keyword) | Q(content__contains=keyword) | Q(
+                            item__name__contains=keyword)
+                    )
+                else:
+                    data = {
+                        'detail': '올바른 sort를 입력해주세요'
+                    }
+                    raise exceptions.ValidationError(data)
+            else:
+                return Review.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        if not self.get_queryset():
+            data = {
+                'detail': '검색결과가 없습니다'
+            }
+            return Response(data)
+        return self.list(request, *args, **kwargs)
--- app/reviews/urls/apis.py
+++ app/reviews/urls/apis.py
@@ -6,4 +6,7 @@ app_name = 'reviews'
 urlpatterns = [
     path('', apis.ReviewListCreateView.as_view(), name='reviews-list'),
     path('<int:review_pk>/', apis.ReviewRetrieveUpdateDestroyView.as_view(), name='reviews-detail'),
+
+    # search
+    path('search/', apis.ReviewSearchFromKeyword.as_view(), name='reviews-search'),
 ]
