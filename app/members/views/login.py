273006c06a8685b3921d9ab4f5ded061b7a664e1
rainsound-k <rainsound128@gmail.com>
2018-06-23 13:50:15 +0900

    login error alert 추가

--- app/members/views/login.py
+++ app/members/views/login.py
@@ -1,5 +1,7 @@
-from django.contrib.auth import authenticate, login
-from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, get_user_model
+from django.shortcuts import redirect, render, render_to_response
+
+User = get_user_model()
 
 __all__ = (
     'login_view',
@@ -14,8 +16,17 @@ def login_view(request):
         email = request.POST['email']
         password = request.POST['password']
         user = authenticate(request, email=email, password=password)
-
-        if user is not None:
+        if not user:
+            if User.objects.filter(email=email):
+                context = {
+                    'error': '비밀번호가 틀렸습니다',
+                }
+            else:
+                context = {
+                    'error': '없는 이메일입니다. 회원가입 해주세요',
+                }
+            return render(request, 'members/login.html', context)
+        else:
             login(request, user)
             # next url 있을 경우 next url로 redirect 설정
             if next_url:
