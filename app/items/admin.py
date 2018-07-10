d019e6afa96e603e7aadf098928ba7c4f91cee85
rainsound-k <rainsound128@gmail.com>
2018-07-10 16:16:30 +0900

    item admin에 img 추가

--- app/items/admin.py
+++ app/items/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Item, ItemComment
 
-admin.site.register(Item)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['photo_tag', 'name', 'price']
+
+    def photo_tag(self, item):
+        if item.img:
+            return mark_safe(f'<img src="{item.img.url}" style="width: 75px;"/>')
+        return None
+
+
 admin.site.register(ItemComment)
